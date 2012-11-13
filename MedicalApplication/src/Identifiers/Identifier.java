b63f9f8d46833e936fc36144de59a9ddd71fd423
Raymond Wainman <raywainman@gmail.com>
2012-11-13 14:11:11 -0700

    Adding Identifier.java

---
+++ MedicalApplication/src/Identifiers/Identifier.java
@@ -0,0 +1,41 @@
+package Identifiers;
+
+public class Identifier {
+	private String name;
+	private String id;
+	private char type;
+
+	public Identifier(String id, String name, char type) {
+		this.name = name;
+		this.id = id;
+		this.type = type;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public char getType() {
+		return this.type;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setId(String Id) {
+		this.id = Id;
+	}
+
+	public void setType(char type) {
+		this.type = type;
+	}
+
+	public String toString() {
+		return this.id + ": " + this.name;
+	}
+}
